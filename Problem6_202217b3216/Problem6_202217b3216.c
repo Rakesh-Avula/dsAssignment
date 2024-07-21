69e4a7251492a773d5e3da384dc81f0391b5b866
Rakesh-Avula <202217b3216@wilp.bits-pilani.ac.in>
2024-07-21 11:13:25 +0530

    Update Problem6_202217b3216.c

--- Problem6_202217b3216/Problem6_202217b3216.c
+++ Problem6_202217b3216/Problem6_202217b3216.c
@@ -1,40 +1,51 @@
-#include<stdio.h> 
-#include<conio.h> 
-#include<stdlib.h>
-void main() 
-{
-int n,a[10],b[10],t[10],w[10],g[10],i,m; 
-float att=0,awt=0;
-//  clrscr(); 
-for(i=0;i<10;i++) 
-{ 
-a[i]=0; b[i]=0; w[i]=0; g[i]=0; 
-} 
-printf("enter the number of process"); 
-scanf("%d",&n); 
-printf("enter the burst times"); 
-for(i=0;i<n;i++) 
-scanf("%d",&b[i]); 
-printf("\nenter the arrival times"); 
-for(i=0;i<n;i++) 
-scanf("%d",&a[i]); 
-g[0]=0; 
-for(i=0;i<10;i++) 
-g[i+1]=g[i]+b[i]; 
-for(i=0;i<n;i++) 
-{ 
-w[i]=g[i]-a[i]; 
-t[i]=g[i+1]-a[i]; 
-awt=awt+w[i]; 
-att=att+t[i]; 
-} 
-awt =awt/n; 
-att=att/n; 
-printf("\n\tprocess\twaiting time\tturn arround time\n"); 
-for(i=0;i<n;i++) 
-{ 
-printf("\tp%d\t\t%d\t\t%d\n",i,w[i],t[i]); 
-} 
-printf("the average waiting time is %f\n",awt); printf("the average turn around time is %f\n",att); 
-getch(); 
+#include <stdio.h>
+ 
+int main() {
+    int n, a[10], b[10], t[10], w[10], g[11], i; // Increase g[] size to 11 for valid access
+    float att = 0, awt = 0; // Declare awt and att as floats for average calculations
+ 
+    // Initialize arrays and variables
+    for (i = 0; i < 10; i++) {
+        a[i] = 0;
+        b[i] = 0;
+        w[i] = 0;
+        g[i] = 0;
+    }
+ 
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+ 
+    printf("Enter the burst times:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &b[i]);
+    }
+ 
+    printf("\nEnter the arrival times:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+ 
+    g[0] = 0;
+    for (i = 0; i < n; i++) {
+        g[i + 1] = g[i] + b[i];
+    }
+ 
+    for (i = 0; i < n; i++) {
+        w[i] = g[i] - a[i];
+        t[i] = g[i + 1] - a[i];
+        awt += w[i];
+        att += t[i];
+    }
+ 
+    awt /= n; // Calculate average waiting time
+    att /= n; // Calculate average turnaround time
+    printf("\n\tProcess\tWaiting Time\tTurnaround Time\n");
+    for (i = 0; i < n; i++) {
+        printf("\tP%d\t\t%d\t\t%d\n", i, w[i], t[i]);
+    }
+ 
+    printf("\nThe average waiting time is %f\n", awt);
+    printf("The average turnaround time is %f\n", att);
+ 
+    return 0;
 }
